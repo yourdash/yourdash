81a51ec3d9ce3e0660b98d3bcb4c06b5bd7a3c54
ewsgit <ewsgit@ewsgit.uk>
2025-08-19 14:40:10 +0100

    reorganise css thingies

--- remake/packages/uikit-mv3-solid/src/components/buttons/FilledButton/UKFilledButton.tsx
+++ remake/packages/uikit-mv3-solid/src/components/buttons/FilledButton/UKFilledButton.tsx
@@ -4,6 +4,60 @@ import type { ButtonShape } from "../shape.ts";
 import { css } from "solid-styled-components";
 import dpToRem from "../../../core/dp.ts";
 import theme from "../../../core/theme.ts";
+import clsx from "clsx";
+
+function getClasses(size: ButtonSize, shape: ButtonShape) {
+    let classNames: string[] = [];
+
+    classNames.push(css`
+        background-color: ${theme.sys.color.primary()};
+        color: ${theme.sys.color["on-primary"]()};
+        transition: all ${theme.sys.motion["duration-300"]()}
+            ${theme.sys.motion.easing.standard.normal()};
+    `);
+
+    switch (size) {
+        case "xs":
+            break;
+        case "s":
+            classNames.push(css`
+                height: ${dpToRem(40)};
+                border-width: ${dpToRem(1)};
+                padding-left: ${dpToRem(16)};
+                padding-right: ${dpToRem(16)};
+            `);
+
+            switch (shape) {
+                case "round":
+                    classNames.push(css`
+                        border-radius: ${theme.sys.shape.corner.full()};
+
+                        &:focus-visible,
+                        &:active {
+                            border-radius: ${theme.sys.shape.corner.medium["default-size"]()};
+                        }
+                    `);
+                    break;
+                case "square":
+                    classNames.push(css`
+                        border-radius: ${theme.sys.shape.corner.medium["default-size"]()};
+
+                        &:focus-visible,
+                        &:active {
+                            border-radius: ${theme.sys.shape.corner.full()};
+                        }
+                    `);
+            }
+        case "m":
+            break;
+        case "l":
+            break;
+        case "xl":
+            break;
+    }
+
+    return classNames;
+}
 
 const UKFilledButton: Component<{
     children: string;
@@ -12,50 +66,7 @@ const UKFilledButton: Component<{
     shape?: ButtonShape;
 }> = ({ children, onClick, size = "s", shape = "round" }) => {
     return (
-        <button
-            class={css`
-                background-color: ${theme.sys.color.primary()};
-                color: ${theme.sys.color["on-primary"]()};
-                transition: all ${theme.sys.motion["duration-300"]()}
-                    ${theme.sys.motion.easing.standard.normal()};
-
-                ${(() => {
-                    switch (size) {
-                        case "xs":
-                        case "s":
-                            return `
-height: ${dpToRem(40)};
-border-width:${dpToRem(1)};
-padding-left: ${dpToRem(16)};
-padding-right: ${dpToRem(16)};
-
-${
-    shape === "round"
-        ? `
-border-radius:${theme.sys.shape.corner.full()};
-
-&:focus-visible, &:active {
-    border-radius: ${theme.sys.shape.corner.medium["default-size"]()};
-}
-`
-        : `
-border-radius:${theme.sys.shape.corner.medium["default-size"]()};
-
-&:focus-visible, &:active {
-    border-radius: ${theme.sys.shape.corner.full()};
-}
-`
-}
-`;
-                        case "m":
-                        case "l":
-                        case "xl":
-                            return "";
-                    }
-                })()}
-            `}
-            onClick={onClick}
-        >
+        <button class={clsx(getClasses(size, shape))} onClick={onClick}>
             {children}
         </button>
     );
